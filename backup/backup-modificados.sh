624cab65f3ef03a08375c1944e947531e3b08648
davidochobits <davidochobits@yahoo.com>
2016-05-30 13:28:23 +0200

    Añado variable DIRECTORIO
    Añado la variable DIRECTORIO que utiliza el comando pwd

--- backup/backup-modificados.sh
+++ backup/backup-modificados.sh
@@ -5,11 +5,12 @@
 #Declaración de variable IFS
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
+DIRECTORIO=$(pwd)
 
 #Utilizamos el comando find para buscar los ficheros modificados en el último día
 #La salida se almacenará en el fichero resultado.txt
 
-find /home/usuario/ -type f -mtime +0  -mtime -1  > resultado.txt
+find DIRECTORIO -type f -mtime +0  -mtime -1  > resultado.txt
 
 #Hemos buscando los ficheros modificados dentro de la carpeta de usuario en el 
 #último día.
@@ -17,7 +18,7 @@ find /home/usuario/ -type f -mtime +0  -mtime -1  > resultado.txt
 #A continuación leemos el fichero y generamos un ficheros comprimido con los 
 #ficheros modificados.
 
-tar cvf /home/usuario/scripts/backup.tar resultado.txt
+tar cvf DIRECTORIO backup.tar resultado.txt
 
 for i in $(cat resultado.txt); do
           tar rvf /home/usuario/scripts/backup.tar "$i"
